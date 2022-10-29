2a8c077a223314905b0d066ecae8bc3f81b9961f
Ali Safarpour <safar2222@yahoo.com>
2022-10-29 12:25:47 +0330

    init project

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const withFonts = require('next-fonts');
+const withPlugins = require('next-compose-plugins');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = withPlugins([withFonts], nextConfig,)
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
-import '../styles/globals.css'
+import store, {persistor} from '../app/store'
+import {Provider} from 'react-redux'
+import {ThemeProvider} from '@mui/material/styles';
+import {CacheProvider} from '@emotion/react';
+import globalTheme from '../themes/globalTheme';
+import createEmotionCache, {cacheRtl} from '../css/createEmotionCache';
+import {Layout} from "../src/sections/Layout";
+import {PersistGate} from "redux-persist/integration/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp(props) {
+    const {Component, emotionCache = clientSideEmotionCache, pageProps} = props;
+    return (
+
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <CacheProvider value={emotionCache}>
+                    <CacheProvider value={cacheRtl}>
+                        <ThemeProvider theme={globalTheme}>
+                            <Layout>
+                                <Component {...pageProps} />
+                            </Layout>
+                        </ThemeProvider>
+                    </CacheProvider>
+                </CacheProvider>
+            </PersistGate>
+        </Provider>
+
+    )
 }
 
 export default MyApp
